86b9be44ae5786e13626c53a5b137040b2ec2a8a
DenysT0707 <denist0707@gmail.com.com>
2022-06-15 09:43:29 +0300

    reupload HW

--- HW_JS/HW_JS.js
+++ HW_JS/HW_JS.js
@@ -12,16 +12,16 @@ let item_5 = item_3
 let item_6 = 15
 let item_6_type = typeof(item_6)
 console.log("item_6 =", item_6,"item_6_type =",item_6_type)
-let item_7 = (item_6 = String(item_6));
+let item_7 = String(item_6);
 let item_7_type = typeof(item_7)
 console.log("item_7 =", item_7,"item_7_type =",item_7_type)
 
-let age_1 = 61
+let age_1 = 32452345
 let age_2 = 18
 let age_3 = 60
 if (age_1 < age_2){
     console.log( "You don’t have access cause your age is " + age_1 + " It’s less then", age_2)
-}else if (age_1>=age_2, age_3>age_1) {
+}else if (age_1>=age_2 && age_3>age_1) {
     console.log('Welcome  !')
 }else if(age_1>age_3) {
     console.log('Keep calm and look Culture channe')
